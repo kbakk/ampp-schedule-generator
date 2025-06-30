1161ca9a913de9665253c97fa5ea60575b644c02
Kristoffer Bakkejord <kbakk@users.noreply.github.com>
2025-06-30 14:43:43 +0200

    refactor: structure cli [wip]

--- generate.py
+++ generate.py
@@ -2,10 +2,16 @@ import typer
 from ampp_schedule_generator import models
 
 
-def generate_schedule(id: str, name: str) -> None:
-    schedule = models.Schedule(id=id, name=name, items=[])
+def generate_schedule(id: str, name: str) -> models.Schedule:
+    items = []
+    # add items ...
+    return models.Schedule(id=id, name=name, items=items)
+
+
+def generate_and_output_schedule(id: str, name: str) -> None:
+    schedule = generate_schedule(id, name)
     print(schedule.model_dump_json())
 
 
 if __name__ == "__main__":
-    typer.run(generate_schedule)
+    typer.run(generate_and_output_schedule)
